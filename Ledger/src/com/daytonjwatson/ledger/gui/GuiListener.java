600b3382782cc790df217d1eaa1e3ebe4db8249b
DaytonJWatson <45436514+DaytonJWatson@users.noreply.github.com>
2026-01-08 13:44:35 -0700

    bug fix

--- Ledger/src/com/daytonjwatson/ledger/gui/GuiListener.java
+++ Ledger/src/com/daytonjwatson/ledger/gui/GuiListener.java
@@ -6,7 +6,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
-import org.bukkit.event.inventory.InventoryView;
+import org.bukkit.inventory.InventoryView;
 
 public class GuiListener implements Listener {
 	private final GuiManager guiManager;
